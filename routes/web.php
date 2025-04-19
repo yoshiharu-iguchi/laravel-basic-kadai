fcaa1c042a23cac6d789994afd8735746655b5d4
iguchi <iguchi2203@gmail.com>
2025-04-19 20:05:28 +0900

    最初のコミット

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+// ルーティングを設定するコントローラを宣言する
+use App\Http\Controllers\HelloController;
+use App\Http\Controllers\PostController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello',[HelloController::class,'index']);
+
+Route::get('/posts',[PostController::class,'index']);
+
+Route::get('/posts/{id}',[PostController::class,'show']);
